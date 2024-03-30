bc1e22ffe097886569e4efe030154c81a655e615
Anna Kirillova <a.kirillova1812@gmail.com>
2024-03-30 14:11:10 +0400

    Limits for requests were added by time

--- src/main/java/ru/annakirillova/selsuptesttask/CrptApi.java
+++ src/main/java/ru/annakirillova/selsuptesttask/CrptApi.java
@@ -7,21 +7,34 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 public class CrptApi {
 
     private static final String URL = "https://ismp.crpt.ru/api/v3/lk/documents/create";
     private static final int STATUS_CREATED = 201;
+    private static final Gson gson = new GsonBuilder().create();
+
+    private final Semaphore semaphore;
+    private final long restrictionPeriodMillis;
+
+    public CrptApi(TimeUnit timeUnit, int requestLimit) {
+        this.semaphore = new Semaphore(requestLimit);
+        this.restrictionPeriodMillis = timeUnit.toMillis(1);
+    }
 
     public static void main(String[] args) {
         String signature = "sign";
         Document document = new Document();
+        CrptApi crptApi = new CrptApi(TimeUnit.MINUTES, 5);
 
-        sendDocument(document, signature);
+        for (int i = 0; i < 1000; i++) {
+            new Thread(() -> crptApi.sendDocument(document, signature)).start();
+        }
     }
 
-    public static void sendDocument(Document document, String signature) {
-        Gson gson = new GsonBuilder().create();
+    public void sendDocument(Document document, String signature) {
         String json = gson.toJson(document);
 
         HttpRequest request = HttpRequest.newBuilder()
@@ -32,6 +45,9 @@ public class CrptApi {
                 .build();
 
         try (HttpClient httpClient = HttpClient.newHttpClient()) {
+            semaphore.acquire();
+            long timeStart = System.currentTimeMillis();
+
             HttpResponse<String> response = httpClient.send(request, BodyHandlers.ofString());
 
             if (response.statusCode() == STATUS_CREATED) {
@@ -41,6 +57,10 @@ public class CrptApi {
                 System.out.println("Response body: " + response.body());
             }
 
+            long timeEnd = System.currentTimeMillis();
+            long sleepTime = Math.max(restrictionPeriodMillis - (timeEnd - timeStart), 0);
+            Thread.sleep(sleepTime);
+            semaphore.release();
         } catch (Exception e) {
             System.out.println("Error happened upon request!");
             e.printStackTrace();
